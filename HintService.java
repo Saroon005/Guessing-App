615649ee057a615994289494038484943deae041
Saroon005 <aps05saroon@gmail.com>
2026-02-23 16:01:27 +0530

    Coded UC3 to print Hints during Wrong Guesses

--- HintService.java
+++ HintService.java
@@ -18,11 +18,11 @@ public class HintService {
      * revealing the exact number.
      */
     public static String generateHint(int target, int hintCount) {
-        if (hintCount == 2) {
+        if (hintCount == 1) {
             return (target % 2 ==0)
                     ? "Hint: Number is Even"
                     : "Hint: Number is ODD";
-        } else if (hintCount ==1) {
+        } else if (hintCount == 2) {
             return (target > 50)
                     ? "Hint: Number is greater than 50"
                     : "Hint: Number is 50 or less";
